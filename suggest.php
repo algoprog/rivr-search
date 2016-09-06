200c0cd237a817cc78686c4ee94fec5f388bc5f2
Chris Samarinas <algoprog@users.noreply.github.com>
2016-09-06 23:59:11 +0300

    Update suggest.php

--- suggest.php
+++ suggest.php
@@ -8,7 +8,7 @@ $q = clean_text(mb_strtolower(htmlspecialchars(urldecode($_GET['q']))));
 
 $date = time();
 
-$query = $db->query("SELECT query, type FROM queries WHERE query LIKE '$q%' ORDER BY count+count/POW(2,($date-first_date)/21600) DESC;");
+$query = $db->query("SELECT query, type FROM queries WHERE query LIKE '$q%' ORDER BY count+count/POW(2,($date-GREATEST(first_date,$date - 300000))/21600) DESC;");
 
 while($data = $query->fetch_assoc()){
 	$suggestions[] = $data['query'];
